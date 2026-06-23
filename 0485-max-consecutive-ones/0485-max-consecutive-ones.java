4009d4a4df369cbd56b46da26f7979ec0bce34af
PRIYANSHU <23priyanshu2005@gmail.com>
2026-06-23 10:55:55 +0530

    Time: 3 ms (60.77%), Space: 52.7 MB (25.92%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -11,7 +11,7 @@ class Solution {
                 right++;
                 maxcount=Math.max(count,maxcount);
             }
-            else if(nums[right]!=1){
+            else{
                 count=0;
                 right++;
             }
